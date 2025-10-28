4555498c4df348601cdef37257210ea79aaeb118
LowHypenE <bandarivihaan50@outlook.com>
2025-10-27 22:03:41 -0400

    Updated title, and alert.

--- public/alert.js
+++ public/alert.js
@@ -1,4 +1,4 @@
 window.onload = function() {
-  alert("CHANGE TRANSPORT OPTION TO BARE, BELOW FOR THE PROXY NOT TO CRASH!             Steps: 1. Click on the transport dropdown 2. Click on the 'Bare' transport option 3. Youre set!");
+  alert("CHANGE TRANSPORT OPTION TO BARE, BELOW FOR THE PROXY NOT TO CRASH! Steps: 1. Click on the transport dropdown 2. Click on the 'Bare' transport option 3. Youre set!");
   alert("This is Willow V-3.1 semi version of unofficial Willow-V4");
 };
